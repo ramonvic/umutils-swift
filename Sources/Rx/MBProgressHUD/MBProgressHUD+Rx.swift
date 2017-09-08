d1bf740474565573292bdacae967236d43f095d3
Ramon Vicente <ramonvicentesilva@hotmail.com>
2017-09-08 13:47:25 -0300

    Added Subpod MBProgressHUD_Rx

---
+++ Sources/Rx/MBProgressHUD/MBProgressHUD+Rx.swift
@@ -0,0 +1,71 @@
+//
+//  MBProgressHUD+Rx.swift
+//  Pods
+//
+//  Created by Ramon Vicente on 16/03/17.
+//
+//
+
+
+import RxSwift
+import RxCocoa
+import MBProgressHUD
+
+private var MBProgressHUDAttachedViewKey: UInt8 = 0
+
+extension Reactive where Base: MBProgressHUD {
+    
+    public var animating: AnyObserver<Bool> {
+        return AnyObserver {event in
+            MainScheduler.ensureExecutingOnScheduler()
+            
+            switch (event) {
+            case .next(let value):
+                if self.base.attachedView == nil {
+                    self.base.attachedView = UIApplication.shared.windows.first!
+                }
+                if self.base.attachedView != nil {
+                    
+                    if value {
+                        let loadingNotification = MBProgressHUD.showAdded(to: self.base.attachedView!, animated: true)
+                        loadingNotification.mode = self.base.mode
+                        loadingNotification.label.text = self.base.label.text
+                        loadingNotification.backgroundView.style = self.base.backgroundView.style
+                    } else {
+                        MBProgressHUD.hide(for: self.base.attachedView!, animated: true)
+                    }
+                }
+            case .error(let error):
+                let error = "Binding error to UI: \(error)"
+                #if DEBUG
+                    fatalError(error)
+                #else
+                    print(error)
+                #endif
+            case .completed:
+                break
+            }
+        }
+    }
+}
+
+extension MBProgressHUD {
+    
+    public static var rx: Reactive<MBProgressHUD>.Type {
+        get {
+            return Reactive<MBProgressHUD>.self
+        }
+        set {
+            // this enables using Reactive to "mutate" base type
+        }
+    }
+    
+    var attachedView: UIView? {
+        get {
+            return objc_getAssociatedObject(self, &MBProgressHUDAttachedViewKey) as? UIView
+        }
+        set(value) {
+            objc_setAssociatedObject(self, &MBProgressHUDAttachedViewKey, value, .OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+}
--- UMUtils-Swift.podspec
+++ UMUtils-Swift.podspec
@@ -1,37 +1,45 @@
 Pod::Spec.new do |spec|
 
     spec.name         = "UMUtils-Swift"
-    spec.version      = "0.1.0"
+    spec.version      = "0.2.0"
     spec.summary      = "Utility Class Library"
     spec.homepage     = "https://github.com/ramonvic"
     spec.license      = { :type => "MIT", :file => "LICENSE.md" }
     spec.author       = { "Ramon Vicente" => "ramonvicentesilva@hotmail.com" }
     spec.platform     = :ios, '9.0'
     spec.source       = { 
-        :git => "https://github.com/ramonvic/UMUtils-Swift.git", 
-        :tag => "0.1.0" }
+        :git => "https://github.com/ramonvic/umutils-swift.git", 
+        :tag => "0.2.0" }
     spec.requires_arc = true
 
     spec.default_subspec = 'Core'
 
     spec.subspec 'Core' do |ss|
         ss.source_files = 'Sources/Core/**/*.swift'
+    end
 
-        ss.dependency 'MBProgressHUD', '~> 1.0.0'
+    spec.subspec 'Material' do |ss|
+        ss.source_files = 'Sources/Material/**/*.swift'
+
+        ss.dependency 'UMUtils-Swift/Core'
         ss.dependency 'Material', '~> 2.0'
-        
     end
 
     spec.subspec 'Rx' do |ss|
-        ss.source_files = 'Sources/Rx/**/*.swift'
+        ss.source_files = 'Sources/Rx/*.swift'
         
         ss.dependency 'UMUtils-Swift/Core'
         ss.dependency 'RxSwift', '~> 3.1'
         ss.dependency 'RxCocoa', '~> 3.1'
         ss.dependency 'RxOptional', '~> 3.1'
         ss.dependency 'RxSwiftUtilities', '~> 1.0.1'
-        ss.dependency 'RxDataSources', '~> 1.0'
-        ss.dependency 'AIFlatSwitch', '~> 1.0.1'
+    end
+
+    spec.subspec 'MBProgressHUD_Rx' do |ss|
+        ss.source_files = 'Sources/Rx/MBProgressHUD/**/*.swift'
+
+        ss.dependency 'UMUtils-Swift/Rx'
+        ss.dependency 'MBProgressHUD', '~> 1.0'
     end
 
     spec.subspec 'APIClient' do |ss|
